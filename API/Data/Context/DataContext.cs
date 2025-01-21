dc6e580b599ea218733c97af37c4a8000f638795
SamehWally <samehwaly27@gmail.com>
2025-01-21 22:14:33 +0200

    AddDbContextAndUsersController

---
+++ API/Data/Context/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data.Context
+{
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
